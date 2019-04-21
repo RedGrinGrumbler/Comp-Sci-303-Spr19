f9220e0a5e2684c975a96f281da98833f1367449
bradstarnes <Scds1029!$!>
2019-04-21 13:24:54 -0500

    Began the creation of the file reader file and created the voidConvertMorseCode function and the process of opening the file

--- Proj2_FileReader.cpp
+++ Proj2_FileReader.cpp
@@ -1,1 +1,20 @@
+/*
+ *   This part of the project reads in a text file that contains morse
+ *   code.
+ *
+ */
 
+#include <fstream>
+#include <iosstream>
+#include <sstream>
+using namespace std;
+
+
+/*  The following function opens a file that contains morse code
+ *  and converts each line of the file.
+*/
+void convertMorseCode(){
+    ifstream morseCodeFile; //Sets the morseCodeFile as an input of the file
+    morseCodeFile.open("team7morsecode.txt"); //Opens the .txt file
+    string morseLine = "";
+}
