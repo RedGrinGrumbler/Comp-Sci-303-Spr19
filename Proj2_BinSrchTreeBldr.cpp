6ee067721e4f3a0e127da5d9370c13ace3843fac
Adam C Smith <36318507+RedGrinGrumbler@users.noreply.github.com>
2019-04-23 22:35:59 -0500

    Making Node Struct and Constructor

--- Proj2_BinSrchTreeBldr.cpp
+++ Proj2_BinSrchTreeBldr.cpp
@@ -1,1 +1,24 @@
+#include <iostream>
+#include <vector>
+
+using namespace std; 
+  
+struct Node 
+{ 
+    char alpha; //Data
+    vector<char> morse; //Key
+    Node* left, * right; 
+}; 
+  
+Node* newNode(vector<char> morse) 
+{ 
+    Node* node = (Node*)malloc(sizeof(Node)); 
+  
+    alpha = morse[0];
+    morse.erase(morse.begin());//Remove the Leading CHAR
+  
+    node->data = morse; 
+    node->left = node->right = NULL; //Standard Children are NULL
+    return (node); 
+}
 
