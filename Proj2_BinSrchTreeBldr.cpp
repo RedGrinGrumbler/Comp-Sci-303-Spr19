c18c09ee6ec8511c707064096fa475dd868d4820
Adam C Smith <36318507+RedGrinGrumbler@users.noreply.github.com>
2019-04-23 23:00:29 -0500

    Added Insert Function for Nodes
    Have not tested this yet. 
    Pulled Inspiration from https://www.geeksforgeeks.org/insert-a-node-in-binary-search-tree-iteratively/

--- Proj2_BinSrchTreeBldr.cpp
+++ Proj2_BinSrchTreeBldr.cpp
@@ -14,11 +14,54 @@ Node* newNode(vector<char> morse)
 { 
     Node* node = (Node*)malloc(sizeof(Node)); 
   
-    alpha = morse[0];
+    char alpha = morse[0]; //Data Value
     morse.erase(morse.begin());//Remove the Leading CHAR
   
-    node->data = morse; 
+    node->data = alpha;
+    node->morseKey = morse; //Key Value
     node->left = node->right = NULL; //Standard Children are NULL
     return (node); 
 }
 
+
+Node* insert(Node* root, vector<char> morseKey) 
+{ 
+    // Create a new Node 
+    Node* theNewNode = newNode(morseKey); 
+  
+    // Declare pointer to start traversing root 
+    Node* x = root; 
+  
+    // Trailing pointer of x 
+    Node* y = NULL; 
+  
+    while (x != NULL) { 
+        y = x; 
+        if (x->morseKey == '.') 
+            x = x->left; 
+        else if (x->morseKey == '_')
+            x = x->right; 
+        else 
+            cout << "Error with Morse Input @ " << y->data << endl << break;//Error Catching Function
+    } 
+  
+    // If the ROOT is NULL, theNewNode == ROOT node 
+    if (y == NULL) 
+        y = theNewNode; 
+  
+    // If the new key ends w/ '.', yNode->left == theNewNode;
+    else if (y->morseKey == '.') 
+        y->left = theNewNode; 
+  
+    // Else If the new key ends w/ '_', yNode->right == theNewNode; 
+    else if (y->morseKey == '_')
+        y->right = theNewNode; 
+  
+    //Error Catching Function
+    else 
+            cout << "Error with Morse Input @ " << y->data << endl << break;
+  
+    // Points to where the new node is inserted 
+    return y; 
+} 
+  
