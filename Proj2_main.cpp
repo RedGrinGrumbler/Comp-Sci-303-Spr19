a314b80fc317b08153b779f90edda023f6cf89ac
bradstarnes <Scds1029!$!>
2019-05-02 12:18:46 -0500

    Reading in morse.txt file and running through each line in the file. Created as function and calling function in main.cpp

--- Proj2_main.cpp
+++ Proj2_main.cpp
@@ -1,1 +1,25 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
 
+int openMorseFile(){
+    ifstream morseCodeFile ("morse.txt");
+    string line;
+    if (morseCodeFile.is_open())
+    {
+        while ( getline (morseCodeFile,line) )
+        {
+            cout << line << '\n';
+        }
+        morseCodeFile.close();
+    }
+}
+
+
+int main() {
+    openMorseFile();
+
+
+    return 0;
+}
